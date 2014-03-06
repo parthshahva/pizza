3b738f1e2834304d12415b768320f9a234ba4d8c
Parth Shah <parthshahva@gmail.com>
2014-03-06 11:05:53 -0600

    initialized topping passed

--- pizza.rb
+++ pizza.rb
@@ -2,4 +2,10 @@ class Pizza
 end
 
 class Topping
+  attr_reader :name, :vegetarian
+  def initialize(name, vegetarian=true)
+    @name = name
+    @vegetarian = vegetarian
+
+  end
 end
--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -17,7 +17,12 @@ describe Topping do
 
       expect(topping.name).to eq('olives')
     end
+        it 'sets whether or not the topping is vegetarian' do
+        topping = Topping.new('bell peppers', vegetarian=true)
 
-end
+         expect(topping.vegetarian).to eq(true)
+        end
+
+    end
 end
 
